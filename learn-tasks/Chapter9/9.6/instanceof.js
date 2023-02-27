bf3fb349951b02879182d6f2116915a0cc575ea8
TimurDavlet <timkoootim@yandex.ru>
2023-02-27 23:36:05 +0300

    Chapter9

---
+++ learn-tasks/Chapter9/9.6/instanceof.js
@@ -0,0 +1,12 @@
+// Почему instanceof в примере ниже возвращает true? Мы же видим, что a не создан с помощью B().
+
+function A() {}
+function B() {}
+
+A.prototype = B.prototype = {};
+
+let a = new A();
+
+alert( a instanceof B ); // true
+
+// instanceof не учитывает саму функцию при проверке, а только prototype, который проверяется на совпадения в прототипной цепочке.
